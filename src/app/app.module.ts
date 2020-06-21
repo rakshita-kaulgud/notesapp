a099b666316e63b37d567eb132d3afc17664bd37
rakshita-kaulgud <rakshita.kaulgud@gmail.com>
2020-06-21 00:22:43 -0400

    First commit to notes app

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NotesListComponent } from './pages/notes-list/notes-list.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NotesListComponent,
+    MainLayoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
